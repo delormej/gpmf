b41a896a2e792514b11fc72e09f90ddc19965e1f
Jason De Lorme <jason@ockam.io>
2018-08-19 19:47:57 -0400

    Adding debug statement to see if realloc actually gets called>

--- demo/GPMF_demo.c
+++ demo/GPMF_demo.c
@@ -66,6 +66,7 @@ uint32_t GetGPMField(const char* field, GPMF_stream* ms, measurement* m) {
 		{
 			tmpbuffer = realloc(tmpbuffer, buffersize);
 			biggestbuffer = buffersize;
+			printf("New realloc is: %i\n", buffersize);
 		}
 
 		if (tmpbuffer && samples)
