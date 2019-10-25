ff94f9042b7f5ec2a860e5376181fe28afc31f0f
ms-jasondel <44730230+ms-jasondel@users.noreply.github.com>
2019-10-24 20:00:09 -0400

    Moving to a new version of the gcc container, which now works on the debian minimal dotnet container.

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ rm -rf build
 mkdir -p build/ 
 
 # run a container to build the gpmf app
-docker run --rm -v "$PWD"/:/gpmf -w /gpmf/demo gcc:4.9 make
+docker run --rm -v "$PWD"/:/gpmf -w /gpmf/demo gcc:9.2 make
 # copy make output binary to local dir
 cp ./demo/gpmfdemo ./build
